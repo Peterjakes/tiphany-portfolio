d39b639e8e4a4b9717ca60d4b2c0d19a23c56382
Peter Jakes <kiberujakes@gmail.com>
2026-08-19 15:40:41 +0300

    feat: added images.js linking IMG keys to real photo and logo files

---
+++ src/components/images.js
@@ -0,0 +1,32 @@
+// src/images.js
+import beauty1 from "./images/beauty1.jpg";
+import cafeJacket from "./images/cafeJacket.jpg";
+import beach from "./images/beach.jpg";
+import cafeYellow from "./images/cafeYellow.jpg";
+import nails from "./images/nails.jpg";
+import sunlitSmile from "./images/sunlitSmile.jpg";
+import sunlitTank from "./images/sunlitTank.jpg";
+import event from "./images/event.jpg";
+import fashionWhite from "./images/fashionWhite.jpg";
+import blazer from "./images/blazer.jpg";
+import windsorHotel from "./images/Windsor-Hotel.jpg";
+import enkataWatches from "./images/Enkata.jpg";
+import lumiHair from "./images/Lumi-Hair_and_Beauty.jpg";
+import samsungMasterclass from "./images/Samsung.jpg";
+
+export const IMG = {
+  beauty1,
+  cafeJacket,
+  beach,
+  cafeYellow,
+  nails,
+  sunlitSmile,
+  sunlitTank,
+  event,
+  fashionWhite,
+  blazer,
+  windsorHotel,
+  enkataWatches,
+  lumiHair,
+  samsungMasterclass,
+};
