5683db855ce3314ec6a94bf9dd8370951e817c56
Peter Jakes <kiberujakes@gmail.com>
2026-09-15 22:25:07 +0300

    feat: added remaining brand logos to images.js

--- src/components/images.js
+++ src/components/images.js
@@ -1,18 +1,32 @@
-// src/images.js
-import beauty1 from "../images/beauty1.jpg";
-import cafeJacket from "../images/cafe-jacket.jpg";
-import beach from "../images/beach.jpg";
-import cafeYellow from "../images/cafe-yellow.jpg";
-import nails from "../images/nails.jpg";
-import sunlitSmile from "../images/sunlit-smile.jpg";
-import sunlitTank from "../images/sunlit-tank.jpg";
-import event from "../images/event.jpg";
-import fashionWhite from "../images/fashion-white.jpg";
-import blazer from "../images/blazer.jpg";
-import windsorHotel from "../images/Windsor-Hotel.jpg";
-import enkataWatches from "../images/Enkata.jpg";
-import lumiHair from "../images/Lumi-Hair and Beauty.jpg";
-import samsungMasterclass from "../images/Samsung.jpg";
+import beauty1 from "./images/beauty1.jpg";
+import cafeJacket from "./images/cafe-jacket.jpg";
+import beach from "./images/beach.jpg";
+import cafeYellow from "./images/cafe-yellow.jpg";
+import nails from "./images/nails.jpg";
+import sunlitSmile from "./images/sunlit-smile.jpg";
+import sunlitTank from "./images/sunlit-tank.jpg";
+import event from "./images/event.jpg";
+import fashionWhite from "./images/fashion-white.jpg";
+import blazer from "./images/blazer.jpg";
+import windsorHotel from "./images/Windsor-Hotel.jpg";
+import enkataWatches from "./images/Enkata.jpg";
+import lumiHair from "./images/Lumi-Hair_and_Beauty.jpg";
+import samsungMasterclass from "./images/Samsung.jpg";
+
+// Newly added brand logos
+import ytheraScents from "./images/Ythera_Scents.jpg";
+import baristaAndCo from "./images/Barista___Co_.jpg";
+import bellaZuri from "./images/Bella_Zuri.jpg";
+import braidingNairobi from "./images/Braiding_Nairobi.jpg";
+import canvasCosmetics from "./images/canvas-cosmetics-complexion-icon.webp";
+import esheSkin from "./images/Eshe_Skin.jpg";
+import fairdealFurniture from "./images/FairDeal_Furniture.png";
+import glamourQueen from "./images/Glamour_Queen.jpg";
+import nouba from "./images/NoUBA_Cosmetics_Logo.png";
+import pistachio from "./images/Pistachio_by_Masala_Twist.jpg";
+import poshPalace from "./images/Posh_Palace.jpg";
+import queensCorner from "./images/QueensCorner_KE.jpg";
+import waraFragrance from "./images/Wara_fragrance.png";
 
 export const IMG = {
   beauty1,
@@ -29,4 +43,17 @@ export const IMG = {
   enkataWatches,
   lumiHair,
   samsungMasterclass,
+  ytheraScents,
+  baristaAndCo,
+  bellaZuri,
+  braidingNairobi,
+  canvasCosmetics,
+  esheSkin,
+  fairdealFurniture,
+  glamourQueen,
+  nouba,
+  pistachio,
+  poshPalace,
+  queensCorner,
+  waraFragrance,
 };
